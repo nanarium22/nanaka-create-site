4bff730e279174df22ea3d91e19677a970286f3c
nanarium22 <n-iguchi@kyuzituhack.com>
2025-06-05 08:42:09 +0900

    rerere

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,7 +12,7 @@ function Profile() {
           className="profile-image"
         />
         <div className="profile-info">
-          <h3>いぐち ななか</h3>
+          <h3>井口奈々花</h3>
           <p>大学で数学を学んでいます。現在はReactやWeb制作を勉強中！</p>
           <p>趣味は宝石のイラスト制作・動画制作・イベントサイト制作など。</p>
         </div>
